d04d8e7481de4d969b6a474d2e8e090279cb4224
n-240 <237142681@qq.com>
2015-10-22 09:47:53 +0800

    1

--- simpleqrcodelibrary/src/main/java/com/google/zxing/client/android/share/ShareActivity.java
+++ simpleqrcodelibrary/src/main/java/com/google/zxing/client/android/share/ShareActivity.java
@@ -134,7 +134,7 @@ public final class ShareActivity extends Activity {
       switch (requestCode) {
         case PICK_BOOKMARK:
         case PICK_APP:
-          showTextAsBarcode(intent.getStringExtra(Browser.BOOKMARKS_URI.toString()));
+          showTextAsBarcode(intent.getStringExtra(Browser.BookmarkColumns.URL));
           break;
         case PICK_CONTACT:
           // Data field is content://contacts/people/984
