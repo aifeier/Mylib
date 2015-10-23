2a9851dac660a144088057d771e7e4a5c770f2b2
n-240 <237142681@qq.com>
2015-10-23 11:32:34 +0800

    添加单独的音频录制

---
+++ app/src/main/java/demo/picture/AudioRecordActivity.java
@@ -0,0 +1,80 @@
+package demo.picture;
+
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.cwf.app.cwf.R;
+import com.cwf.app.cwflibrary.utils.FileUtils;
+import com.cwf.app.cwflibrary.utils.TimeUtils;
+
+import java.io.IOException;
+
+import lib.BaseActivity;
+
+/**
+ * Created by n-240 on 2015/10/23.
+ */
+public class AudioRecordActivity extends BaseActivity{
+    private Button start;
+    private Button stop;
+
+    private MediaRecorder mediaRecorder;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_videorecord);
+        initView();
+    }
+
+    private void initView(){
+        start = (Button)findViewById(R.id.start);
+        stop = (Button) findViewById(R.id.stop);
+        start.setText("开始录制声音");
+        stop.setEnabled(false);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startRecord();
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopRecord();
+            }
+        });
+    }
+
+
+    private void startRecord(){
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
+        mediaRecorder.setAudioEncodingBitRate(44100);
+        mediaRecorder.setOutputFile(FileUtils.createPath("audio/",
+                TimeUtils.getSimpleDate().replace(" ", "-").replace(":", "-")+".aac"));
+        try {
+            mediaRecorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mediaRecorder.start();
+        start.setEnabled(false);
+        stop.setEnabled(true);
+    }
+
+    private void stopRecord(){
+        if(mediaRecorder!=null){
+            mediaRecorder.stop();
+            mediaRecorder.release();
+            mediaRecorder = null;
+            start.setEnabled(true);
+            stop.setEnabled(false);
+        }
+
+    }
+}
