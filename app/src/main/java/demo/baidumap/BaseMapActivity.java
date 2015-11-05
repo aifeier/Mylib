56abff4c8504442424cf238182f5fc1d53cde912
n-240 <237142681@qq.com>
2015-11-05 08:58:59 +0800

    +++

--- app/src/main/java/demo/baidumap/BaseMapActivity.java
+++ app/src/main/java/demo/baidumap/BaseMapActivity.java
@@ -1,11 +1,28 @@
 package demo.baidumap;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.map.MapView;
 import com.cwf.app.cwf.R;
+import com.handmark.pulltorefresh.library.autoloadlist.AutoLoadAdapter;
+import com.handmark.pulltorefresh.library.autoloadlist.AutoRefreshListView;
+import com.handmark.pulltorefresh.library.autoloadlist.ViewHolder;
+
+import java.util.ArrayList;
 
 import lib.BaseActivity;
+import lib.utils.ActivityUtils;
 
 /**
  * Created by n-240 on 2015/9/25.
@@ -13,11 +30,81 @@ import lib.BaseActivity;
 public class BaseMapActivity extends BaseActivity{
     private MapView mMapView;
 
+    private AutoRefreshListView<BDLocation> autoRefreshListView;
+    private AutoLoadAdapter<BDLocation> autoLoadAdapter;
+    private ArrayList<BDLocation> arrayList ;
+
+    private static LocationClient mLocClient;
+    private static MyLocationListenner mLocationListenner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.map_base);
+        setContentView(R.layout.layout_pulltorefresh);
         setTitle("百度地图Demo");
-        mMapView = (MapView)findViewById(R.id.mapview);
+//        mMapView = (MapView)findViewById(R.id.mapview);
+        arrayList = new ArrayList<BDLocation>();
+        autoRefreshListView = (AutoRefreshListView<BDLocation>) findViewById(R.id.pull_refresh_list);
+        autoLoadAdapter = new AutoLoadAdapter<BDLocation>(this, android.R.layout.simple_list_item_1) {
+            @Override
+            public void buildView(ViewHolder holder, BDLocation data) {
+                ((TextView) holder.findViewById(android.R.id.text1)).setText(data.getTime()+"\n"
+                +data.getLatitude()+", " + data.getLongitude());
+            }
+
+            @Override
+            public void getPage(int page) {
+
+            }
+        };
+        autoRefreshListView.setListAdapter(autoLoadAdapter);
+        autoRefreshListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(mLocClient!=null) {
+//                    mLocClient.requestLocation();
+                    mLocClient.requestNotifyLocation();
+//                    mLocClient.requestOfflineLocation();
+                }
+            }
+        });
+
+        mLocClient = new LocationClient(this);
+
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true);
+        option.setCoorType("gcj02");
+        option.setScanSpan(1000 * 10);
+        option.disableCache(true);
+        mLocClient.setLocOption(option);
+        option.setPriority(LocationClientOption.GpsFirst);
+        mLocClient.setLocOption(option);
+        mLocationListenner = new MyLocationListenner();
+        mLocClient.registerLocationListener(mLocationListenner);
+        mLocClient.start();
+
+    }
+
+    public class MyLocationListenner implements BDLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            if (location != null ) {
+                autoLoadAdapter.resetAdapterAndRefresh();
+                arrayList.add(0, location);
+                autoLoadAdapter.setmData(arrayList, autoRefreshListView);
+                autoRefreshListView.onRefreshComplete();
+                return;
+            }
+
+        }
+
+        public void onReceivePoi(BDLocation poiLocation) {
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mLocClient.unRegisterLocationListener(mLocationListenner);
+        mLocClient.stop();
     }
 }
