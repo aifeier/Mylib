c48ff4de265f6f62467f254a6803780fe941f2ca
n-240 <237142681@qq.com>
2015-11-27 11:20:50 +0800

    同步

--- app/src/main/java/demo/Html5Activity.java
+++ app/src/main/java/demo/Html5Activity.java
@@ -33,6 +33,7 @@ public class Html5Activity extends Activity{
         webSettings.setJavaScriptEnabled(true);
 //        webSettings.setUseWideViewPort(true);
         webSettings.setSupportZoom(true);
+        webSettings.setLoadsImagesAutomatically(true);
  /*       webSettings.setAppCacheEnabled(true);
         webSettings.setAppCachePath(SDCardUtils.getAbleDirectoryPath());*/
 
