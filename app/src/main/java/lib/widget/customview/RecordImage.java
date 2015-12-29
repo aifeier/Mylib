25999c6859b843bbf7a69a6e0521a3aa4d1e46c4
n-240 <237142681@qq.com>
2015-12-29 10:39:10 +0800

    添加自定义按钮

---
+++ app/src/main/java/lib/widget/customview/RecordImage.java
@@ -0,0 +1,62 @@
+package lib.widget.customview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.Button;
+
+import com.cwf.app.cwf.R;
+
+/**
+ * Created by n-240 on 2015/12/29.
+ *
+ * @author cwf
+ */
+public class RecordImage extends Button{
+    private OnRecordTouchListener onRecordTouchListener;
+    public RecordImage(Context context) {
+        super(context);
+    }
+
+    public RecordImage(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RecordImage(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(onRecordTouchListener!=null){
+            switch (event.getAction()){
+                case MotionEvent.ACTION_DOWN:
+                    setBackgroundResource(R.drawable.but_item_select);
+                    onRecordTouchListener.OnActionDown();
+                    return true;
+                case MotionEvent.ACTION_MOVE:
+                    onRecordTouchListener.OnActionMove();
+                    return true;
+                case MotionEvent.ACTION_UP:
+                    onRecordTouchListener.OnActionUp();
+                    return true;
+            }
+        }
+        return super.onTouchEvent(event);
+    }
+
+    public OnRecordTouchListener getOnRecordTouchListener() {
+        return onRecordTouchListener;
+    }
+
+    public void setOnRecordTouchListener(OnRecordTouchListener onRecordTouchListener) {
+        this.onRecordTouchListener = onRecordTouchListener;
+    }
+
+    public interface OnRecordTouchListener{
+        public void OnActionDown();
+        public void OnActionMove();
+        public void OnActionUp();
+    }
+
+}
