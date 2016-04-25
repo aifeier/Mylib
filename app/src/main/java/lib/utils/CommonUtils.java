4d6ca8f71303919258e04f3c3eb61686dff82165
n-240 <237142681@qq.com>
2016-04-25 19:01:06 +0800

    添加判断是否有虚拟键的方法

--- app/src/main/java/lib/utils/CommonUtils.java
+++ app/src/main/java/lib/utils/CommonUtils.java
@@ -463,6 +463,30 @@ public class CommonUtils {
     }
 
 
+    //获取是否存在NavigationBar/虚拟键
+    public static boolean checkDeviceHasNavigationBar(Context context) {
+        boolean hasNavigationBar = false;
+        Resources rs = context.getResources();
+        int id = rs.getIdentifier("config_showNavigationBar", "bool", "android");
+        if (id > 0) {
+            hasNavigationBar = rs.getBoolean(id);
+        }
+        try {
+            Class systemPropertiesClass = Class.forName("android.os.SystemProperties");
+            Method m = systemPropertiesClass.getMethod("get", String.class);
+            String navBarOverride = (String) m.invoke(systemPropertiesClass, "qemu.hw.mainkeys");
+            if ("1".equals(navBarOverride)) {
+                hasNavigationBar = false;
+            } else if ("0".equals(navBarOverride)) {
+                hasNavigationBar = true;
+            }
+        } catch (Exception e) {
+
+        }
+        return hasNavigationBar;
+
+    }
+
 
 
 }
