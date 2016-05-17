3ed2ab90222530b295dfbd063ce91c145a1f3b25
n-240 <237142681@qq.com>
2016-05-17 18:28:43 +0800

    DrawerLayout

---
+++ app/src/main/java/demo/custom/test/DrawerLayoutActivity.java
@@ -0,0 +1,58 @@
+package demo.custom.test;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.Window;
+
+import com.cwf.app.cwf.R;
+
+import lib.BaseActivity;
+
+/**
+ * Created by n-240 on 2016/5/17.
+ *
+ * @author cwf
+ * @email 237142681@qq.com
+ */
+public class DrawerLayoutActivity extends BaseActivity {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        useToolbar = false;
+        setContentView(R.layout.activity_drawer_layout);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.add, R.string.del);
+        mDrawerLayout.setDrawerListener(mActionBarDrawerToggle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mActionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mActionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mActionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+}
